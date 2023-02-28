a3d1c56dcff3c74389defc7309ff5dcff957028e
MonikaBhasin7 <monikabhasin.sd17@gmail.com>
2023-03-01 00:26:10 +0530

    state hoist

--- app/src/main/java/com/hk/jetpackcompose3/MainActivity.kt
+++ app/src/main/java/com/hk/jetpackcompose3/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hk.jetpackcompose3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,8 +14,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,6 +22,8 @@ import com.hk.jetpackcompose3.ui.theme.JetpackCompose3Theme
 import com.hk.jetpackcompose3.ui.theme.Purple200
 
 class MainActivity : ComponentActivity() {
+
+    val mainViewModel by  viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,22 +31,39 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color.White
                 ) {
-                    BaseFun()
+                    BaseFun(viewModel = mainViewModel)
                 }
             }
         }
     }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        JetpackCompose3Theme {
+            BaseFun(mainViewModel)
+        }
+    }
+
+
 }
 
 @Composable
-fun BaseFun() {
+fun BaseFun(viewModel: MainViewModel) {
     var clickCount by remember { mutableStateOf(0) }
+
+    BaseFunUi(clickCount = clickCount, buttonClickCallback = {
+        clickCount++
+    })
+}
+
+@Composable
+fun BaseFunUi(clickCount: Int, buttonClickCallback: () -> Int) {
     Column(
         verticalArrangement = Arrangement.Top,
         modifier = Modifier
-            .background(color = Purple200)
             .padding(horizontal = 4.dp, vertical = 4.dp)
             .fillMaxSize()
     ) {
@@ -56,24 +74,10 @@ fun BaseFun() {
             color = Color.Green,
         )
         Button(onClick = {
-            clickCount++;
+            buttonClickCallback()
         }, shape = RoundedCornerShape(8.dp)) {
             Text(text = "Tap")
         }
-        Button(onClick = {
-            clickCount++;
-        }, shape = RoundedCornerShape(8.dp)) {
-            Text(text = "Tap")
-        }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackCompose3Theme {
-        BaseFun()
     }
 }
 
---
+++ app/src/main/java/com/hk/jetpackcompose3/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.hk.jetpackcompose3
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+}
